3e753bb5c991f9a6744ffdeaf255cb1443814b19
Peter McIntyre <peter.mcintyre@senecacollege.ca>
2017-12-08 10:32:40 -0500

    Dedicated IA Server, and an app that uses its security environment

--- Week_09/IAServer/IA/Controllers/Manager.cs
+++ Week_09/IAServer/IA/Controllers/Manager.cs
@@ -111,6 +111,13 @@ namespace IA.Controllers
             claimType = claimType.Trim().ToLower();
             claimValue = claimValue.Trim().ToLower();
 
+            // Special situations for the well-known claims if a short form is submitted
+            claimType = (claimType == "role") ? ClaimTypes.Role : claimType;
+            claimType = (claimType == "name") ? ClaimTypes.Name : claimType;
+            claimType = (claimType == "emailaddress") ? ClaimTypes.Email : claimType;
+            claimType = (claimType == "givenname") ? ClaimTypes.GivenName : claimType;
+            claimType = (claimType == "surname") ? ClaimTypes.Surname : claimType;
+
             // Attempt to fetch the object
             var o = ds.AppClaims
                 .SingleOrDefault(a => a.ClaimType.ToLower() == claimType && a.ClaimValue.ToLower() == claimValue);
